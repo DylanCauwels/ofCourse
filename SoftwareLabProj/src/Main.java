7793ed8eb679e6bb90e114015367aa1f37fc88ac
jw45625 <jw45625@utexas.edu>
2018-11-02 11:18:13 -0500

    connect to Postgres Server

--- SoftwareLabProj/src/Main.java
+++ SoftwareLabProj/src/Main.java
@@ -1,11 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		while(true) {
-			
-		}
+		
+		final String url = "jdbc:postgresql://softwarelabinstance.ct09nasdzura.us-east-1.rds.amazonaws.com/postgres";
+		final String user = "administrator";
+		final String password = "password";
+		
+		Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+ 
+        
 	}
 
 }
