677dbacbd7aff71d6d1a4f11ce52a567ce6717bb
jw45625 <jw45625@utexas.edu>
2018-11-14 03:00:28 -0600

    put database in a singlton design pattern

---
+++ SoftwareLabProj/src/DataBaseConnection.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	final private String url = "jdbc:postgresql://softwarelabinstance.ct09nasdzura.us-east-1.rds.amazonaws.com/postgres";
+	final private String user = "administrator";
+	final private String password = "password";
+	private Connection conn;
+	private DatabaseConnection databaseConnection = new DatabaseConnection();
+	
+	
+	private DatabaseConnection(){
+		conn = null;
+	    try {
+	        conn = DriverManager.getConnection(url, user, password);
+	        System.out.println("Connected to the PostgreSQL server successfully.");
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	    }
+	}
+	
+	public static DatabaseConnection getInstance() {
+		return databaseConnection;
+	}
+	
+}
--- SoftwareLabProj/src/Main.java
+++ SoftwareLabProj/src/Main.java
@@ -7,17 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		final String url = "jdbc:postgresql://softwarelabinstance.ct09nasdzura.us-east-1.rds.amazonaws.com/postgres";
-		final String user = "administrator";
-		final String password = "password";
-		
-		Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+		DatabaseConnection conn = DatabaseConnection.getInstance();
  
         
 	}
